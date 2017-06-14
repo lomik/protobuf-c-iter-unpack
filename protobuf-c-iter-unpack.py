864a2251aa81a8247a6c09d459e512aef91895f4
Dmitry Zimnukhov <zimnuhov@gmail.com>
2017-06-14 13:27:53 +0300

    fix field names that are C++ keywords

--- protobuf-c-iter-unpack.py
+++ protobuf-c-iter-unpack.py
@@ -331,6 +331,18 @@ class _base(object):
   (types which use the varint, 32-bit, or 64-bit wire types) can be declared "packed".
   """
   repeated_init_size = 4
+  keywords = set([
+    "and", "and_eq", "asm", "auto", "bitand", "bitor", "bool", "break", "case",
+    "catch", "char", "class", "compl", "const", "const_cast", "continue",
+    "default", "delete", "do", "double", "dynamic_cast", "else", "enum",
+    "explicit", "extern", "false", "float", "for", "friend", "goto", "if",
+    "inline", "int", "long", "mutable", "namespace", "new", "not", "not_eq",
+    "operator", "or", "or_eq", "private", "protected", "public", "register",
+    "reinterpret_cast", "return", "short", "signed", "sizeof", "static",
+    "static_cast", "struct", "switch", "template", "this", "throw", "true", "try",
+    "typedef", "typeid", "typename", "union", "unsigned", "using", "virtual",
+    "void", "volatile", "wchar_t", "while", "xor", "xor_eq"
+  ])
 
   def __init__(self, protobuf_field):
     self._field = protobuf_field
@@ -382,7 +394,12 @@ class _base(object):
     ]
 
   def name(self):
-    return self._field.name.lower()
+    name = self._field.name.lower()
+
+    if name in self.keywords:
+        return name + '_'
+
+    return name
 
   def repeated_check_resize(self):
     if not self.is_repeated():
